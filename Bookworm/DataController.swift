370511f7a18fec24ec70feddeeb38c80be00a628
Landon Cayia <landon.cayia@gmail.com>
2022-08-04 11:07:32 -0400

    Create Core Data model and controller

---
+++ Bookworm/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Landon Cayia on 8/4/22.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
